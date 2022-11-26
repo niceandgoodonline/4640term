8cc1fdbacb42ce99837ea14d135605ab72b14917
niceAndGoodOnline <felix.ruttan@gmail.com>
2022-11-25 20:21:34 -0800

    pushed with gittyup

--- terraform/ec2/pull_vpc_state.sh
+++ terraform/ec2/pull_vpc_state.sh
@@ -26,12 +26,16 @@ public_array="[\"$(sed "s|,|\",\"|g" public.subnets)\"]"
 echo "private_array : $private_array"
 rm *.subnets
 
+cat "config/$1" >> state.tfvars
+
 sed -i "s|PRIVATE_SUBNETS|$private_array|g" state.tfvars
 sed -i "s|PUBLIC_SUBNETS|$public_array|g" state.tfvars
+
+sed -i "s|PRIV_SUBNET|${private_array[0]}|g" state.tfvars
+sed -i "s|PUB_SUBNET|${public_array[0]}|g" state.tfvars
+
 sed -i "s|VPC_ID|$vpc_id|g" state.tfvars
 sed -i "s|FRONTEND_SG|$frontend_sg|g" state.tfvars
 sed -i "s|DB_SG|$db_sg|g" state.tfvars
 
 cat state.tfvars
-
-cat "config/$1" >> state.tfvars

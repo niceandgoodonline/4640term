c68785dee14c90ef6ff80d6b1ab798bd6c8eccac
niceAndGoodOnline <felix.ruttan@gmail.com>
2022-11-27 11:27:56 -0800

    pushed with gittyup

--- s3/db/ops/config/ec2/db/push_db_state.sh
+++ s3/db/ops/config/ec2/db/push_db_state.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+get_private_ip()
+{
+  TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600"`
+  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4
+}
+
+private_ip=$(get_private_ip)
+
+push_ssm_value "$project/secret/db/root" "$rt_pass"
+push_ssm_value "$path_db_ip" "$private_ip"
+push_ssm_value "$path_db_name" "$db_name"
+push_ssm_value "$path_db_port" "$db_port"
+push_ssm_value "$path_app_user" "$app_user_name"
+push_ssm_value "$path_app_pass" "$app_user_pass"
--- terraform/ec2/userdata/application.sh
+++ terraform/ec2/userdata/application.sh
@@ -6,4 +6,4 @@ sudo apt-get update
 sudo apt-get install -y -q awscli nginx awscli python3 python3-pip libmysqlclient-dev
 sudo aws s3 sync "$repo/app/" "$stage/app"
 sudo aws s3 sync "$repo/util/" "$stage/util"
-# sudo source "$stage/ops/config/ec2/app/bootstrap.sh" | tee "/tmp/setup.log"
+sudo source "$stage/ops/config/ec2/app/bootstrap.sh" | tee "/tmp/setup.log"
